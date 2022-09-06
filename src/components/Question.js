70ded9cbeda5fc2a3d981a41b149a8cb22d1628a
lucafisc <lucafischer@fastmail.com>
2022-09-06 12:09:39 +0200

    Shuffle answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,20 @@
 import Answer from "./Answer";
 import "../styles/Question.css";
 import he from "he";
+import shuffle from "fisher-yates";
 
 export default function Question(props) {
   let question = props.data.question;
   question = he.decode(question);
   const incorrectAnswers = props.data.incorrect_answers;
-  const answers = incorrectAnswers.map((answer) => {
+  const correctAnswer = props.data.correct_answer;
+  const answersArray = shuffle(incorrectAnswers.concat(correctAnswer));
+
+  const answers = answersArray.map((answer) => {
     return <Answer data={answer} />;
   });
 
-  console.log(props.data.question);
+  console.log(props.data);
   return (
     <div className="question">
       <h1>{question}</h1>
