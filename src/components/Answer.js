9882a111c9caf7360a05bcc4f981b28ce94b4b30
lucafisc <lucafischer@fastmail.com>
2022-09-06 16:03:17 +0200

    Add state to questions and answers

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -4,7 +4,7 @@ export default function Answer(props) {
   let answer = props.data;
   answer = he.decode(answer);
   return (
-    <div className="answer">
+    <div className="answer" onClick={() => props.chooseAnswer(answer)}>
       <h2>{answer}</h2>
     </div>
   );
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,20 +1,31 @@
+import React from "react";
 import Answer from "./Answer";
 import "../styles/Question.css";
 import he from "he";
 import shuffle from "fisher-yates";
 
 export default function Question(props) {
-  let question = props.data.question;
-  question = he.decode(question);
+  const [question, setQuestion] = React.useState(
+    he.decode(props.data.question)
+  );
+
   const incorrectAnswers = props.data.incorrect_answers;
   const correctAnswer = props.data.correct_answer;
-  const answersArray = shuffle(incorrectAnswers.concat(correctAnswer));
+  const [answersArray, setanswersArray] = React.useState(
+    shuffle(incorrectAnswers.concat(correctAnswer))
+  );
 
   const answers = answersArray.map((answer) => {
-    return <Answer data={answer} />;
+    return <Answer data={answer} chooseAnswer={chooseAnswer} />;
   });
 
-  console.log(props.data);
+  const [chosenAnswer, setChosenAnswer] = React.useState(false);
+  function chooseAnswer(value) {
+    setChosenAnswer(value);
+    console.log(chosenAnswer);
+  }
+
+  React.useEffect(() => {}, []);
   return (
     <div className="question">
       <h1>{question}</h1>
